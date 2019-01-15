7cdb273d8cf012c5fc608633783f665c7737f908
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-01-15 12:49:26 +0900

    spec2.x

---
+++ src/spec2/x/Ex1.java
@@ -0,0 +1,39 @@
+package spec2.x;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ex1
+ */
+public class Ex1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ex1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charshet=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("Servlet example - Spec 3.0 or more");
+		out.print("<br />");
+		out.print("1. When you register your servlet, use web.xml ");
+		
+		out.close();
+	}
+
+}
